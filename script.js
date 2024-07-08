3838ffdc290e380db73db5163669b2b1db91c3c2
Samuel Umoh <umohsamuel337@gmail.com>
2024-07-08 03:06:48 +0100

    new

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 var header = document.getElementById('header');
+var scrollBtn = document.getElementById('scroll-btn');
+
 
 // When the user scrolls down 3000px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-header.style.backgroundColor = "#071903";
-} else {
-header.style.backgroundColor = "transparent";
-}
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+        header.style.backgroundColor = "#071903";
+    } else {
+        header.style.backgroundColor = "transparent";
+    }
+
+    if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
 }
